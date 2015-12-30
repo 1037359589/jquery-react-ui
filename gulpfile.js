3c628b341dbb5e592d34682f4ae1eec11e0c28f0
bll <bll@blldeMacBook-Pro.local>
2015-12-30 11:34:43 +0800

    NO-BG

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp=require('gulp');
+var react=require('gulp-react');
+var babel=require('gulp-babel');
 var uglify=require('gulp-uglify');
 var concat=require('gulp-concat');
 var jquery=require('gulp-jquery');
 var rename=require('gulp-rename');
 var minCss=require('gulp-minify-css');
+
 
 gulp.task('jquery-ui-js',function(){
 	return gulp.src('./js/jquery-ui/*.js')
@@ -14,20 +17,35 @@ gulp.task('jquery-ui-js',function(){
 			.pipe(uglify())
 			.pipe(gulp.dest('./dest/jquery-ui'));
 });
-gulp.task('jquery-ui-css',function(){
+gulp.task('own-ui-css',function(){
 	return gulp.src('./css/jquery-ui/*.css')
 			// .pipe(jquery())
-			.pipe(concat('jquery-own-ui.css'))
-			.pipe(gulp.dest('./dest/jquery-ui'))
+			.pipe(concat('jquery-ui.css'))
+			.pipe(gulp.dest('./dest/own-ui'))
 			.pipe(rename({suffix:'.min'}))
 			.pipe(minCss())
-			.pipe(gulp.dest('./dest/jquery-ui'));
+			.pipe(gulp.dest('./dest/own-ui'));
 });
-gulp.task('default',['jquery-ui-css'],function(){
+gulp.task('react-ui-js',function(){
+	return gulp.src('./js/react-ui/*.js')
+			.pipe(react())
+			// .pipe(babel({
+			// 	presets:['babel-preset-es2015']	
+			// }))
+			.pipe(concat('react-own-ui.js'))
+			.pipe(gulp.dest('./dest/react-ui'))
+			.pipe(rename({ suffix:'.min' }))
+			.pipe(uglify())
+			.pipe(gulp.dest('./dest/react-ui'));
+});
+gulp.task('default',['own-ui-css','jquery-ui-js','react-ui-js'],function(){
 	gulp.watch('./js/jquery-ui/*.js',['jquery-ui-js']).on('change',function(e){
 		console.log('File'+e.path+'was'+e.type+'runing tasks...');
 	});
-	gulp.watch('./css/jquery-ui/*.css',['jquery-ui-css']).on('change',function(e){
+	gulp.watch('./js/react-ui/*.js',['react-ui-js']).on('change',function(e){
+		console.log('File'+e.path+'was'+e.type+'runing tasks...');
+	});
+	gulp.watch('./css/jquery-ui/*.css',['own-ui-css']).on('change',function(e){
 		console.log('File'+e.path+'was'+e.type+'runing tasks...');
 	})
 	console.log('压缩合并完成！！');
---
+++ js/react-ui/react-button.js
@@ -0,0 +1,8 @@
+var Btn=React.createClass({
+	render:function(){
+		return (
+			<div>你好，世界！！</div>
+		)
+	}
+});
+ReactDOM.render(<Btn/>,document.getElementById('app'));
