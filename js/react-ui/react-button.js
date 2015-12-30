36061c87a78490a9a2e59285bc79d0634b936124
bll <bll@blldeMacBook-Pro.local>
2015-12-30 14:14:53 +0800

    reactUI初始化

--- js/react-ui/react-button.js
+++ js/react-ui/react-button.js
@@ -1,8 +1,37 @@
-var Btn=React.createClass({
+
+var BtnDiv=React.createClass({
 	render:function(){
 		return (
-			<div>你好，世界！！</div>
+			<div>
+			<BtnDefault/>
+			<BtnSmall/>
+			</div>
 		)
 	}
 });
-ReactDOM.render(<Btn/>,document.getElementById('app'));
+var BtnDefault=React.createClass({
+	render:function(){
+		return (
+			<button className="btn default normal hover" id="btn">
+				DEFAULT
+			</button>
+		)
+	}
+});
+var BtnSmall=React.createClass({
+	render:function(){
+		return (
+			<button className="btn default small hover">
+				DEFAULT
+			</button>
+		)
+	}
+});
+ReactDOM.render(<BtnDiv/>,document.getElementById('app'));
+(function(win,document){
+	var  ReactUi=function(btn){
+		this.btn=document.getElementById(btn);
+		console.log(this.btn);
+	}
+	var ReactUi=new ReactUi('btn');
+})(window,document)
