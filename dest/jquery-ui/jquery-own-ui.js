f0d12e53f55bf2cfcce8873f52b5c6fc79cc6bef
bll <bll@blldeMacBook-Pro.local>
2016-01-01 20:35:08 +0800

    fade插件修改

--- dest/jquery-ui/jquery-own-ui.js
+++ dest/jquery-ui/jquery-own-ui.js
@@ -6,6 +6,7 @@
 * @delay:多长时间切换一次
 * @nextEvent:下一张图显示后的回调函数
 * @prevEvent:上一张显示后的回调函数
+* @indexEvent:确定显示某一项触发回调函数
 * */
 (function($,window,document){
     var FadeSlide=function(ele,options){
@@ -25,7 +26,7 @@
             timeOut:""
         };
         that.opt= $.extend({},that.default,options);
-
+        /*循环计时*/
         that.interval=function(){
                 that.opt.inter=setInterval(function(){
                     that.opt.activeIndex++;
@@ -36,18 +37,34 @@
                 },that.opt.delay);
                 return this;
             };
+        /*定位某个之后延时继续循环*/
         that.opt.delayGoing=function(){
             that.opt.timeOut=setTimeout(function(){
                 that.interval();
             },that.opt.delay);
         };
+        /*初始化*/
         that.init=function(){
             that.opt.activeIndex=0;
             that.nodes.slideNext.show();
             that.nodes.slideItemList.find("li").eq(that.opt.activeIndex).fadeIn(that.opt.speed);
             that.nodes.slideItemIndex.find("li").eq(that.opt.activeIndex).addClass("index-active");
-            that.interval().next(that.opt.nextEvent).prev(that.opt.prevEvent);
+            that.interval().next(that.opt.nextEvent).prev(that.opt.prevEvent).indexClick();
         };
+        /*直接定位第几个*/
+        that.indexClick=function(){
+            that.nodes.slideItemIndex.find("li").each(function(index,dom){
+                var $dom=$(dom);
+                $dom.on("click",function(){
+                    clearInterval(that.opt.inter);
+                    clearTimeout(that.opt.timeOut);
+                    that.opt.activeIndex=index;
+                    that.opt.delayGoing();
+                    that.show(index,that.opt.indexEvent);
+                })
+            });
+        };
+        /*显示后面*/
         that.next=function(fn){
             that.nodes.slideNext.on("click",function(){
                 clearInterval(that.opt.inter);
@@ -62,6 +79,7 @@
             });
             return this;
         };
+        /*显示前面*/
         that.prev=function(fn){
              that.nodes.slidePrev.on("click",function(){
                  clearInterval(that.opt.inter);
@@ -74,7 +92,9 @@
                  }
                  that.show(that.opt.activeIndex,fn);
              });
+            return this;
         };
+        /*显示*/
         that.show=function(index,fn){
             that.nodes.slideItemList.find("li").hide();
             that.nodes.slideItemList.find("li").eq(index).fadeIn(that.opt.speed);
