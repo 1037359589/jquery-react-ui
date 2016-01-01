6ffdfeeeb499f2770a2214dd27f0fdf6034b0552
bll <bll@blldeMacBook-Pro.local>
2016-01-01 17:48:43 +0800

    轮播插件fade

--- js/jquery-ui/test1.js
+++
@@ -1,9 +0,0 @@
-function Person(name,age,sex){
-	this.name=name;
-	this.age=age;
-	this.sex=sex;
-	console.log(this.name,this.age,this.sex);
-}
-var p=new Person('own','18','男');
-console.log(jQuery);
-console.log(124443);
