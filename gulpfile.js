5f8f3fbb659c3bb81a7d8071141dba522086a2cd
bll <bll@blldeMacBook-Pro.local>
2016-01-05 12:52:50 +0800

    tuisong

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,19 @@ var minCss=require('gulp-minify-css');
 // var es2015=require('babel-preset-es2015');
 
 
-gulp.task('jquery-ui-js',function(){
-	return gulp.src('./js/jquery-ui/*.js')
+gulp.task('ui-js',function(){
+	return gulp.src('./js/ui/*.js')
 			// .pipe(jquery())
-			.pipe(concat('jquery-own-ui.js'))
-			.pipe(gulp.dest('./dest/jquery-ui'))
+			.pipe(concat('own-ui.js'))
+			.pipe(gulp.dest('./dest/ui'))
 			.pipe(rename({suffix:'.min'}))
 			.pipe(uglify())
-			.pipe(gulp.dest('./dest/jquery-ui'));
+			.pipe(gulp.dest('./dest/ui'));
 });
 gulp.task('own-ui-css',function(){
-	return gulp.src('./css/jquery-ui/*.css')
+	return gulp.src('./css/ui/*.css')
 			// .pipe(jquery())
-			.pipe(concat('jquery-ui.css'))
+			.pipe(concat('own-ui.css'))
 			.pipe(gulp.dest('./dest/own-ui'))
 			.pipe(rename({suffix:'.min'}))
 			.pipe(minCss())
@@ -31,22 +31,22 @@ gulp.task('react-ui-js',function(){
 	return gulp.src('./js/react-ui/*.js')
 			.pipe(react())
 			// .pipe(babel({
-			// 	presets:['babel-preset-es2015']	
+			// 	presets:['babel-preset-es2015']
 			// }))
 			.pipe(concat('react-own-ui.js'))
 			.pipe(gulp.dest('./dest/react-ui'))
-			.pipe(rename({ suffix:'.min' }))
-			.pipe(uglify())
-			.pipe(gulp.dest('./dest/react-ui'));
+			.pipe(rename({ suffix:'.min' }));
+//			.pipe(uglify())
+//			.pipe(gulp.dest('./dest/react-ui'));
 });
-gulp.task('default',['own-ui-css','jquery-ui-js','react-ui-js'],function(){
-	gulp.watch('./js/jquery-ui/*.js',['jquery-ui-js']).on('change',function(e){
+gulp.task('default',['own-ui-css','ui-js','react-ui-js'],function(){
+	gulp.watch('./js/ui/*.js',['ui-js']).on('change',function(e){
 		console.log('File'+e.path+'was'+e.type+'runing tasks...');
 	});
 	gulp.watch('./js/react-ui/*.js',['react-ui-js']).on('change',function(e){
 		console.log('File'+e.path+'was'+e.type+'runing tasks...');
 	});
-	gulp.watch('./css/jquery-ui/*.css',['own-ui-css']).on('change',function(e){
+	gulp.watch('./css/ui/*.css',['own-ui-css']).on('change',function(e){
 		console.log('File'+e.path+'was'+e.type+'runing tasks...');
 	})
 	console.log('压缩合并完成！！');
